bf113c1b334ff018c55be54800bb1d5bba15fead
Namit2924 <namitdhangar5@gmail.com>
2026-04-28 22:47:26 +0530

    Update app.js file

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const app = express();
 
 const PORT = 3000;
 
+// Version (change this to test CI/CD)
+const VERSION = "v1.0";
+
 app.get("/", (req, res) => {
-    res.send("🚀 Node.js CI/CD App Running Successfully!");
+    res.send(`
+        <h1>🚀 Node.js CI/CD App</h1>
+        <p><b>Status:</b> Running Successfully ✅</p>
+        <p><b>Version:</b> ${VERSION}</p>
+        <p><b>Time:</b> ${new Date().toLocaleString()}</p>
+        <p><b>Server:</b> EC2 Auto Deployment</p>
+    `);
 });
 
 app.listen(PORT, () => {
